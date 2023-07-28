b84825d11fe16e4f61950d6f71ef87c54907a304
SmallMistake <piersquaredup@gmail.com>
2023-07-28 00:28:02 -0500

    Upgraded how rooms are handled. Upgraded Camera System

--- Assets/Scripts/Interactables/Lockable.cs
+++ Assets/Scripts/Interactables/Lockable.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Lockable : MonoBehaviour
 {
     public bool unlocked;
+
+    public UnityEvent onUnlock;
 
     public bool TryToUnlock()
     {
         if (!unlocked)
         {
             unlocked = !unlocked;
+            onUnlock?.Invoke();
         }
         return unlocked;
     }
